beb283232f9d332b15795792a132ed8f86b41a34
SebastianDte <sebateunmate@outlook.com>
2025-10-24 15:42:57 -0300

    feat(auth): mejorar flujo y pantallas de autenticación (login, registro, recuperación)

--- app/auth/screen/index.ts
+++ app/auth/screen/index.ts
@@ -1,2 +1,5 @@
+//index.ts
 export { default as Login } from './Login'
 export { default as Register } from './Register'
+export { default as ForgotPassword } from './ForgotPassword'; 
+export { default as NewPassword } from './NewPassword';
